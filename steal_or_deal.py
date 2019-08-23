e770c7cd3df0d968e2ec738dc76a145b6fcf6899
juniorkfelix <felixonsare@gmail.com>
2019-08-24 00:53:19 +0300

    made changes upto chek point 4

--- steal_or_deal.py
+++ steal_or_deal.py
@@ -6,28 +6,47 @@
 # This is my own work as defined by the University’s
 # Academic Misconduct policy.
 #
+import random
 
+list = ["Steal", "Deal"]
+player_choice = input("Would you like to play Steal or Deal [y|n]?")
 
-choice = input("Would you like to play Steal or Deal [y|n]?")
-
-if(choice == "y"):
+if(player_choice == "y"):
 	print("Jackpot: 200")
-	choice2 = input("Steal, Deal or Quit [S|D|Q]? S")
+	choice2 = input("Steal, Deal or Quit [S|D|Q]?")
+	
 
 	if (choice2 == "s"):
-		print("Steal")
+#		computer_choice = random.choice(list)
+		computer_choice = random.randint(1,2)
+#		print(computer_choice)
+		
+		if (computer_choice==1):
+			computer_choice = "Steal"
+
+		elif(computer_choice==2):
+			computer_choice="Deal"
+
+		print("C:  Steal  |  ",computer_choice)
 
 	elif (choice2 == "d"):
-		print("Deal")
+		computer_choice = random.randint(1,2)
+
+		if (computer_choice==1):
+			computer_choice = "Steal"
+
+		elif(computer_choice==2):
+			computer_choice="Deal"
+		print("C:  Deal  |  ",computer_choice)
 
 	elif (choice2 == "q"):
-		print("Quit")
+		print("No worries... another time perhaps... :) ")
 
 	else:
 		print("invalid choice")
 
 
-elif(choice == "n"):
+elif(player_choice == "n"):
 	print("No worries... another time perhaps... :)")
 
 else:
